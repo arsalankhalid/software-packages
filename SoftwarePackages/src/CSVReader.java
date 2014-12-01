93b943ba08e17ffdc96406b2e2c5b21581e163a2
Pavlo Kuzhel <pavlick@gmail.com>
2014-11-30 19:40:15 -0500

    add a check to query to make sure added edge is with a goal source

--- SoftwarePackages/src/CSVReader.java
+++ SoftwarePackages/src/CSVReader.java
@@ -12,86 +12,86 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.TreeMap;
 import java.util.TreeSet;
- 
+
 public class CSVReader {
 
-  public ArrayList<String> getArrayList(String url, int field) throws IOException {
-	URL stockURL = new URL(url);
-	
-	BufferedReader br = new BufferedReader(new InputStreamReader(stockURL.openStream()));
+	public ArrayList<String> getArrayList(String url, int field) throws IOException {
+		URL stockURL = new URL(url);
 
-	String line = "";
-	String cvsSplitBy = ",";
-	ArrayList<String> list = new ArrayList<String>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(stockURL.openStream()));
 
-	while ((line = br.readLine()) != null) {
-		// use comma as separator
-		String[] string = line.split(cvsSplitBy);
-		list.add(string[field]);
+		String line = "";
+		String cvsSplitBy = ",";
+		ArrayList<String> list = new ArrayList<String>();
+
+		while ((line = br.readLine()) != null) {
+			// use comma as separator
+			String[] string = line.split(cvsSplitBy);
+			list.add(string[field]);
+		}
+
+		br.close();
+
+		return list;
 	}
 
-	br.close();
-	
-	return list;
-  }
-  
-  public TreeMap<String,String> getMap(String url) throws IOException {
-	URL stockURL = new URL(url);
-	
-	BufferedReader br = new BufferedReader(new InputStreamReader(stockURL.openStream()));
+	public TreeMap<String,String> getMap(String url) throws IOException {
+		URL stockURL = new URL(url);
 
-	String line = "";
-	String cvsSplitBy = ",";
-	TreeMap<String,String> map = new TreeMap<String,String>();
-	
-	while ((line = br.readLine()) != null) {
-		// use comma as separator
-		String[] string = line.split(cvsSplitBy);
-		map.put(string[0], string[1]);
- 	}
+		BufferedReader br = new BufferedReader(new InputStreamReader(stockURL.openStream()));
 
-	br.close();
+		String line = "";
+		String cvsSplitBy = ",";
+		TreeMap<String,String> map = new TreeMap<String,String>();
 
-	return map;
-  }
-  
-  public TreeMap<String,String> getMapSet(String url) throws IOException {
-	  
-	URL stockURL = new URL(url);
-	
-	BufferedReader br = new BufferedReader(new InputStreamReader(stockURL.openStream()));
+		while ((line = br.readLine()) != null) {
+			// use comma as separator
+			String[] string = line.split(cvsSplitBy);
+			map.put(string[0], string[1]);
+		}
 
-	String line = "";
-	String cvsSplitBy = ",";
-	TreeMap<String,String> map = new TreeMap<String,String>();
-	
-	while ((line = br.readLine()) != null) {
-		String[] string = line.split(cvsSplitBy);
- 		map.put(string[0], string[1]);
- 	}
-	
-	br.close();
+		br.close();
 
-	return map;
-  }
-  
-  public TreeSet<String> getTreeSet(String url, int index) throws IOException{
-  	URL stockURL = new URL(url);
-	
-	BufferedReader br = new BufferedReader(new InputStreamReader(stockURL.openStream()));
-
-	String line = "";
-	String cvsSplitBy = ",";
-	TreeSet<String> set = new TreeSet<String>();
-
-	while ((line = br.readLine()) != null) {
-		// use comma as separator
-		String[] string = line.split(cvsSplitBy);
-		set.add(string[index]);
+		return map;
 	}
 
-	br.close();
+	public TreeMap<String,String> getMapSet(String url) throws IOException {
 
-	return set;
-  }
+		URL stockURL = new URL(url);
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(stockURL.openStream()));
+
+		String line = "";
+		String cvsSplitBy = ",";
+		TreeMap<String,String> map = new TreeMap<String,String>();
+
+		while ((line = br.readLine()) != null) {
+			String[] string = line.split(cvsSplitBy);
+			map.put(string[0], string[1]);
+		}
+
+		br.close();
+
+		return map;
+	}
+
+	public TreeSet<String> getTreeSet(String url, int index) throws IOException{
+		URL stockURL = new URL(url);
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(stockURL.openStream()));
+
+		String line = "";
+		String cvsSplitBy = ",";
+		TreeSet<String> set = new TreeSet<String>();
+
+		while ((line = br.readLine()) != null) {
+			// use comma as separator
+			String[] string = line.split(cvsSplitBy);
+			set.add(string[index]);
+		}
+
+		br.close();
+
+		return set;
+	}
 }
--- SoftwarePackages/src/Query.java
+++ SoftwarePackages/src/Query.java
@@ -6,53 +6,53 @@ import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class Query {
-	  public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException{
 		Graph graph = new Graph();
-		
+
 		String goalsCSV = "http://england.proximity.on.ca/chris/outbound/lb/lb_goal.csv",
 				depCapCSV = "http://england.proximity.on.ca/chris/outbound/lb/lb_dep_cap.csv",
 				capBinCSV = "http://england.proximity.on.ca/chris/outbound/lb/lb_dep_rpm.csv",
 				binDepCSV = "http://england.proximity.on.ca/chris/outbound/lb/lb_dep_srpm.csv"
 				;
-		
+
 		TreeSet<String> goals = new TreeSet<String>();
 		TreeMap<String,String> capabilityBinaries = new TreeMap<String,String>();
 		ArrayList<String> sources = new ArrayList<String>();
 		ArrayList<String> capabilities = new ArrayList<String>();
-		
+
 		ArrayList<String> binary = new ArrayList<String>();
 		ArrayList<String> binSources = new ArrayList<String>();
-		
+
 		TreeMap<String, TreeSet<String>> sourceBinaries = new TreeMap<String, TreeSet<String>>();
-		
+
 		CSVReader reader = new CSVReader();
-		
+
 		goals = reader.getTreeSet(goalsCSV, 0);
 		sources = reader.getArrayList(depCapCSV, 0);
 		capabilities = reader.getArrayList(depCapCSV, 1);
 
 		binary = reader.getArrayList(binDepCSV, 0);
 		binSources = reader.getArrayList(binDepCSV, 1);
-		
+
 		// extract all binaries that are true for a source and put them in a map of TreeSet objects
 		for(int i=0; i<binSources.size(); i++){
 			TreeSet<String> binarySet = new TreeSet<String>();
-			
+
 			for(int j=i; j<binSources.size(); j++){
 				if (binSources.get(i).equals(binSources.get(j))){
 					binarySet.add(binary.get(j));
 				}
 			}
-			
+
 			sourceBinaries.put(binSources.get(i), binarySet);
 		}
-		
+
 		System.out.println("Size of the Map with TreeSets: " + sourceBinaries.size());
-		
+
 		Set<String> keys = sourceBinaries.keySet();
-		
+
 		java.util.Iterator<String> iterator = keys.iterator();
-		
+
 		capabilityBinaries = reader.getMap(capBinCSV);
 
 		/*
@@ -60,7 +60,7 @@ public class Query {
 		 */
 		int counter=0;
 		iterator = goals.iterator();
-		
+
 		while(iterator.hasNext()){
 			String goal = iterator.next();
 			//System.out.println("Checking for goal" + goal);
@@ -70,30 +70,30 @@ public class Query {
 					//System.out.println("Found capabilities for goal: " + capabilities.get(i));
 					String capab = capabilities.get(i);
 					String bin = capabilityBinaries.get(capab);
-					
+
 					for (Map.Entry<String,TreeSet<String>> entry : sourceBinaries.entrySet()) {
 						TreeSet<String> bins = entry.getValue();
-						if (bins.contains(bin) ){//&& goals.contains(entry.getKey()) ){
-						   graph.addEdge(goal, entry.getKey());
-						   counter++;
+						if (bins.contains(bin) && goals.contains(entry.getKey()) ){
+							graph.addEdge(goal, entry.getKey());
+							counter++;
 						}
 					}
-					
+
 				}
 			}
 		}
 		System.out.println("Total Edges added: " + counter);
-		
+
 		int edges, verts;
 		verts = graph.getNumberOfVertices();
 		edges = graph.getNumberOfEdges();
 		System.out.println("Number of Vertices: " + verts + "\nNumber of Edges: " + edges);
-		
+
 		graph.printEdges("dbus", "dbus");
-		
+
 		graph.printPath("devel", "devel", 4);
-		
+
 		System.out.println("Main Finished");
-		
-	  }
+
+	}
 }
